df901e09dc6c8581ae2c42a9d3dcc8aed382cc22
kuosi <dev@kuosi.io>
2021-12-16 08:18:54 +0000

    add vulnerability

---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+import pymongo
+import motor
+import jsonschema
+import attr
+import attr
+import certifi
+import charset_normalizer
+import docopt
+import idna
+import pipreqs
+import pyrsistent
+import urllib3
+import yarg
+import pkg_resources
+import pytz
+import sqlparse
+
+
+print("Hello World")
